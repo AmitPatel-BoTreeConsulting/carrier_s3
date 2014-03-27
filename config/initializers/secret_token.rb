4ea37a264497313258fd35fabaff6370a6a24fc5
AmitPatel-BoTreeConsulting <amit.patel@botreeconsulting.com>
2014-03-27 18:16:14 +0530

    Upload image on local file storage

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CarrierS3::Application.config.secret_token = '815d8945447c88f60b2658a8dd27275aba59dadcdc4c07eb6998d9ccd395db3820ac5d9a2daa9c935d6f0563af2f3ff92ad4f3c8b376c31f3706c60690ee60ae'
